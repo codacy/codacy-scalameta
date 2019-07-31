a0fa091f0a69540adca79c7af30981dd0ce824c1
Luís Ferreira <contact@lsferreira.net>
2019-07-31 09:49:54 +0100

    tests: fix unittest for Custom_Scala_TypeParameterName
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- patterns-base/src/main/resources/docs/tests/Custom_Scala_TypeParameterName.scala
+++ patterns-base/src/main/resources/docs/tests/Custom_Scala_TypeParameterName.scala
@@ -1,13 +1,13 @@
 //#Patterns: Custom_Scala_TypeParameterName
 package docs.tests
 
-//#Issue: {"severity": "Warn", "line": 9, "patternId": "Custom_Scala_TypeParameterName"}
-//#Issue: {"severity": "Warn", "line": 11, "patternId": "Custom_Scala_TypeParameterName"}
-//#Issue: {"severity": "Warn", "line": 18, "patternId": "Custom_Scala_TypeParameterName"}
-//#Issue: {"severity": "Warn", "line": 23, "patternId": "Custom_Scala_TypeParameterName"}
-
+//#Warn: Custom_Scala_TypeParameterName
+//#Warn: Custom_Scala_TypeParameterName
 case class Entry[TKey, TProperties](key: TKey, properties: TProperties)
 
+//#Warn: Custom_Scala_TypeParameterName
+//#Warn: Custom_Scala_TypeParameterName
+//#Warn: Custom_Scala_TypeParameterName
 trait HistoryBuilder[TKey, TEntity <: Option[TProperties], TProperties]
 
 class Response[A]
@@ -15,9 +15,13 @@ class Response[A]
 trait TResponse[T]
 
 trait a {
+  //#Warn: Custom_Scala_TypeParameterName
+  //#Warn: Custom_Scala_TypeParameterName
+  //#Warn: Custom_Scala_TypeParameterName
   def HistoryEncoder[TKey, TEntity <: Option[Int], TProperties]()
 }
 
 case class Cat[T, B](key: T, properties: B)
 
+//#Warn: Custom_Scala_TypeParameterName
 trait Dog[F, G <: Option[String], Milk]
