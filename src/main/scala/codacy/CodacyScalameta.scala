66a12906e2c062d395d03361b0794ec1e0410bf3
José Miguel  Melo <josemiguelmelo94@gmail.com>
2021-06-08 15:09:11 +0100

    bump: Bump codacy scala seed to 5.0.2

--- src/main/scala/codacy/CodacyScalameta.scala
+++ src/main/scala/codacy/CodacyScalameta.scala
@@ -45,12 +45,14 @@ object CodacyScalameta extends Tool {
       configuration match {
         case Some(defs) =>
           defs.flatMap { pDef: Pattern.Definition =>
-            val params: Set[Parameter.Definition] = pDef.parameters.getOrElse {
+            val params: Set[Parameter.Definition] = if (pDef.parameters.isEmpty) {
               specification.patterns
                 .filter(_.patternId == pDef.patternId)
-                .flatMap(_.parameters.toList.flatMap(_.map { spec =>
+                .flatMap(_.parameters.toList.map { spec =>
                   Parameter.Definition(spec.name, spec.default)
-                }))
+                })
+            } else {
+              pDef.parameters
             }
 
             newPatterns
