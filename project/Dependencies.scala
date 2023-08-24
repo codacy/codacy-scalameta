12fc68c9e9f445ef0943dc74cafd100f1b8fda9d
David Marinho <david.marinho@codacy.com>
2023-08-24 17:18:08 +0100

    bump versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val scalaV = "2.13.11"
 
   val betterFiles = "com.github.pathikrit" %% "better-files" % "3.8.0"
-  val codacyEngine = "com.codacy" %% "codacy-engine-scala-seed" % "6.0.1"
+  val codacyEngine = "com.codacy" %% "codacy-engine-scala-seed" % "5.0.2"
   val scalaReflect = "org.scala-lang" % "scala-reflect" % scalaV
 
 }
